ef9cbeea31feaeec108e779d8718d536d6ede2c7
Dmitry Kabarukhin <thedimzone99@gmail.com>
2019-11-19 20:29:34 +0300

    check videos and posts implemented & fixed config

--- default_config.js
+++ default_config.js
@@ -2,10 +2,16 @@
   create same file with name "config.js" and configure it
 */
 
-//https://oauth.vk.com/authorize?client_id=YOUR_APP_CLIENT_ID&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,groups,wall,photos&response_type=token&v=5.52
+//https://oauth.vk.com/authorize?client_id=YOUR_APP_CLIENT_ID&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,groups,wall,photos,video&response_type=token&v=5.52
 exports.token = ""; // VK api token
 exports.target_id = "1"; // user id to stalk
 
-exports.getPhotoInterval = 2000;
-exports.checkInterval = 650;
 exports.show_vk_errors = false;
+
+exports.getphoto_interval = 2000;
+exports.check_interval = 650;
+exports.getwall_interval = 3000;
+exports.getvideos_interval = 4000;
+
+exports.wallposts_count = 1; // (per user)
+//limit of wall.get is 5000 request per day
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
   TODO:
-    1) check wall posts of friends
-    2) check wall posts of target's groups subscribes
-    3) check videos of friends (may be)
+    1) check wall posts of target's groups subscribes
+    vk limits wall.get to 5000 requests per day, so..... (100 friends * 100 groups = 10000 requests)
+    we have to use Streaming API
 */
 
 var request = require('request');
@@ -16,7 +16,7 @@ var req = function(name, params, cb) {
   for(element in params) { params_url += element + "=" + params[element] + "&"; }
   const url = "https://api.vk.com/method/" + name + "?&access_token=" + config.token + "&v=5.103&" + params_url;
 
-  request.get({url: url}, function(err,httpResponse,body) {
+  request.get({url: url, timeout: 20000}, function(err,httpResponse,body) {
     const res = JSON.parse(body);
     if(res["error"]) {
       if(config.show_vk_errors) console.log(res["error"]);
@@ -37,7 +37,7 @@ var check = function() {
 
   if(object) {
     req("likes.isLiked", { type: object["type"], user_id: config.target_id, owner_id: object["owner_id"], item_id: object["id"] }, function(res) {
-      if(res["response"]["liked"] == 1) sendLog("vk.com/" + object["type"] + object["owner_id"] + "_" + object["id"]);
+      if(res["response"]["liked"] == 1) sendLog("vk.com/id" + object["owner_id"] + " - vk.com/" + object["type"] + object["owner_id"] + "_" + object["id"]);
 
       isLastLogAboutEnd = false;
     });
@@ -51,16 +51,26 @@ var check = function() {
 
 var setToBeChecked = function(res) {
   for(item of res["response"]["items"]) {
-    item["type"] = "photo"; // to be changed further for ability of checking all types
+    if(item["post_type"]) {
+      item["type"] = "post";
+    } else if(item["duration"]) {
+      item["type"] = "video";
+    } else {
+      item["type"] = "photo";
+    }
     toBeChecked.push(item);
   }
 }
+
+console.log(config.check_interval);
 
 req("friends.get", { user_id: config.target_id, count: 5000 }, function(res) {
   for(var i = 0; i < res["response"]["count"]; i++) {
     const friend_id = res["response"]["items"][i];
 
     setTimeout(req, config.getphoto_interval * i, "photos.getAll", { owner_id: friend_id, count: 200 }, setToBeChecked);
+    setTimeout(req, config.getwall_interval * i, "wall.get", { owner_id: config.target_id, count: config.wallposts_count, filter: "owner", }, setToBeChecked);
+    setTimeout(req, config.getvideos_interval * i, "video.get", { owner_id: config.target_id, count: 200 }, setToBeChecked);
   }
 
   setInterval(check, config.check_interval);
